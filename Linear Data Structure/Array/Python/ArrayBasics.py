17b9678a50025e5444c9178229d22353639cd549
Rupan Samanta <rvsjan32@gmail.com>
2025-04-21 16:13:24 +0530

    added comments for clarity in ArrayBasics.py

--- Linear Data Structure/Array/Python/ArrayBasics.py
+++ Linear Data Structure/Array/Python/ArrayBasics.py
@@ -1,4 +1,6 @@
+# declaration and initialization of an array
 numbers = [10, 20, 30, 40, 50]
 print("Elements in the array:")
+# printing the elements of the array using a for loop
 for i in range(len(numbers)):
     print(f"Element at index {i}: {numbers[i]}")
