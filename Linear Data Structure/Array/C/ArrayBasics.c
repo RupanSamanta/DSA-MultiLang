dcfd812f8ea756e493892c51330f7d58722dd970
Rupan Samanta <rvsjan32@gmail.com>
2025-04-24 20:28:21 +0530

    added ArrayBasics.c file

---
+++ Linear Data Structure/Array/C/ArrayBasics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing an array
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    // Printing the array
+    printf("Array elements:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
